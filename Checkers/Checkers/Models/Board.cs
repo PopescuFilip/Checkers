1976d4dfdd7c2bc59614973f672991a618c7c68a
FilipPopescu <PopescuFilip@users.noreply.github.com>
2024-04-03 15:42:54 +0300

    added static Board class for Board initialization

---
+++ Checkers/Checkers/Models/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Models
+{
+    public  static class Board
+    {
+        public const int Rows = 8;
+        public const int Cols = 8;
+
+        public static Tile[,] BoardMatrix { get; }
+        static Board()
+        {
+            BoardMatrix = new Tile[Rows, Cols];
+
+            for (int i = 0; i < Rows; i++)
+                for (int j = 0; j < Cols; j++)
+                    BoardMatrix[i, j] = new Tile(i, j);
+        }
+    }
+}
--- Checkers/Checkers/ViewModels/BoardViewModel.cs
+++ Checkers/Checkers/ViewModels/BoardViewModel.cs
@@ -14,18 +14,16 @@ namespace Checkers.ViewModels
 {
     public class BoardViewModel: BaseViewModel
     {
-        public const int Rows = 8;
-        public const int Cols = 8;
         public ObservableCollection<ObservableCollection<Tile>> Board { get; set; }
         public BoardViewModel() 
         {
             Board = new ObservableCollection<ObservableCollection<Tile>>();
 
-            for (int i = 0; i < Rows; i++)
+            for (int i = 0; i < Models.Board.Rows; i++)
             {
                 ObservableCollection<Tile> row = new ObservableCollection<Tile>();
-                for (int j = 0; j < Cols; j++)
-                    row.Add(new Tile(i, j));
+                for (int j = 0; j < Models.Board.Cols; j++)
+                    row.Add(Models.Board.BoardMatrix[i,j]);
                 Board.Add(row);
             }
         }
