569a0aa96f563219b29135e37ca74afa45a99d8e
FilipPopescu <PopescuFilip@users.noreply.github.com>
2024-04-15 16:38:47 +0300

    fixed downloader not updating red and white pieces

--- Checkers/Checkers/Services/GameDownloaderService.cs
+++ Checkers/Checkers/Services/GameDownloaderService.cs
@@ -22,6 +22,8 @@ namespace Checkers.Services
 
             using (StreamReader reader = new StreamReader(dialog.FileName)) 
             {
+                int noOfRedPieces = 0;
+                int noOfWhitePieces = 0;
                 for (int i = 0; i < Board.Rows; i++) 
                 {
                     for (int j = 0; j < Board.Cols; j++)
@@ -29,8 +31,14 @@ namespace Checkers.Services
                         Enums.Color color = TileService.StringToColor(reader.ReadLine());
                         Enums.Type type = TileService.StringToType(reader.ReadLine());
                         game.Board[i][j].Update(color, type);
+                        if(color == Enums.Color.White)
+                            noOfWhitePieces++;
+                        else if(color == Enums.Color.Red)
+                            noOfRedPieces++;
                     }
                 }
+                game.RedPieces = noOfRedPieces;
+                game.WhitePieces = noOfWhitePieces;
                 game.CurrentPlayer = TileService.StringToColor(reader.ReadLine());
                 if(game.CurrentPlayer == Enums.Color.White)
                     game.NonCurrentPlayer = Enums.Color.Red;
