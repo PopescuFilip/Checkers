5e2b0c79c9c09ab178d1a0209074aa32b5a02398
FilipPopescu <PopescuFilip@users.noreply.github.com>
2024-04-16 16:06:48 +0300

    added about message and implemented about command

--- Checkers/Checkers/Commands/AboutCommand.cs
+++ Checkers/Checkers/Commands/AboutCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,20 @@ namespace Checkers.Commands
 {
     public class AboutCommand : CommandBase
     {
+        private const string Path = "../../Resources/About.txt";
         public override void Execute(object parameter)
         {
-            MessageBox.Show("About message");
+            string aboutMessage = string.Empty;
+            using (StreamReader reader = new StreamReader(Path))
+            {
+                string s;
+                while((s = reader.ReadLine()) != null)
+                {
+                    aboutMessage += '\n' + s;
+                }
+            }
+
+            MessageBox.Show(aboutMessage);
         }
     }
 }
